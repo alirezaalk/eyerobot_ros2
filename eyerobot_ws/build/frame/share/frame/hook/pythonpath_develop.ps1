c4412c170b15a92a09c6c3624bdebc332f0cdef0
alirezaalk <alireza.alikhani@tum.de>
2023-03-17 15:17:06 +0100

    add realsense publisher on ros2 running

---
+++ eyerobot_ws/build/frame/share/frame/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/alireza/projects/eyerobot-ros2/eyerobot_ws/build/frame"
--- eyerobot_ws/src/frame/setup.py
+++ eyerobot_ws/src/frame/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "d445_publisher = frame.d445_publisher:main"
         ],
     },
 )
